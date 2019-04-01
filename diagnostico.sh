cdb4d4562ed5bfb7221bafd8d9b63de254c5fd5d
Wesllen <wesllen.maia2010@gmail.com>
2019-04-01 16:59:18 -0300

    Adicionada nova função
    5) Comando Remoto - usa SSH para trazer resultado de comandos remotos

--- diagnostico.sh
+++ diagnostico.sh
@@ -16,27 +16,75 @@
 # Testado em:
 #   bash 4.4.19
 # ------------------------------------------------------------------------ #
+#while read line
+#do
+#    echo $line
+#done < /dir/arq.log | pv -n | dialog --gauge "Lendo log..." 10 70 0
+[ ! -x $(which sshpass) ] && apt install sshpass -y > /dev/null
+[ ! -x $(which nmap) ] && apt install nmap -y > /dev/null
+
+#-------------------------- FUNCOES ----------------------------------------- #
 
 function speed () {
   speedtest=$(speedtest-cli)
-  dialog --stdout --title "-= Teste de Velocidade da Interne =-" --msgbox "$speedtest" 0 0
-
+  dialog \
+  --stdout \
+  --title "-= Teste de Velocidade da Interne =-" \
+  --msgbox "$speedtest" 0 0
 }
 function TamDisco() {
   disco=$(df -h | head -n 1 && df -h | grep sd)
-  dialog --stdout --title " -= Informações sobre os Discos =- " --msgbox "$disco" 0 0
+  dialog --stdout --title " -= Informações sobre os Discos =- " \
+  --msgbox "$disco" 0 0
 }
 function PingMaquina() {
-  ipmaquina=$(dialog --title "-= Ping para IP Destino =-" --stdout --inputbox "Digite o IP Destino:" 0 0)
+  ipmaquina=$(dialog \
+  --title "-= Ping para IP Destino =-" \
+  --stdout \
+  --inputbox "Digite o IP Destino:" 0 0)
   resultado=$(ping -c 3 ${ipmaquina})
-  dialog --title " Resultado do Ping: " --stdout --msgbox "$resultado" 0 0
+  dialog \
+  --title " Resultado do Ping: " \
+  --stdout \
+  --msgbox "$resultado" 0 0
 }
 function NmapScan() {
-  faixaip=$(dialog --title "-= Nmap Scanner =-" --stdout --inputbox " Digite a faixa de IP. Ex: 10.0.1.0/24 " 0 0)
+  faixaip=$(dialog \
+  --title "-= Nmap Scanner =-" \
+  --stdout \
+  --inputbox " Digite a faixa de IP. Ex: 10.0.1.0/24 " 0 0)
   nresultado=$(nmap -sn ${faixaip})
-  dialog --title " Resultado do Ping: " --stdout --msgbox "$nresultado" 0 0
+  dialog \
+  --title " Resultado do Ping: " \
+  --msgbox "$nresultado" 0 0
+}
+function CMDremoto() {
+  maquina=$(dialog \
+  --title "-=HOST=-" \
+  --stdout \
+  --inputbox "Informe o IP do Host:" 0 0)
+  usuario=$(dialog \
+  --title "-=Usuário=-" \
+  --stdout \
+  --inputbox "Informe o Usuário:" 0 0)
+  pwd=$(dialog \
+  --title "-=Confirmação=-" \
+  --stdout \
+  --passwordbox "Digite a Senha do SSH:" 0 0)
+  comando=$(dialog \
+  --title "-=Comando=-" \
+  --stdout \
+  --inputbox "Entre com o Comando:" 0 0)
+  conexao=/usr/bin/ssh
+  cmdext=$(sshpass -p $pwd $conexao $usuario@$maquina "$comando")
+      dialog \
+      --title "Retorno do Comando" \
+      --stdout \
+      --msgbox "$cmdext" 0 0
+
 }
 
+#-------------------------- MENU ------------------------------------------------------ #
 while true;
     do
   menu=$(dialog --stdout --title "Menu Principal" --menu "Escolha uma opção:" 0 0 0 \
@@ -44,6 +92,7 @@ while true;
         2 "Verificar Espaço do Disco"           \
         3 "Ping uma Estação"                    \
         4 "Scanner Nmap"                        \
+        5 "Comando Remoto"                      \
         0 "Sair"                                )
 
         [ $? -ne 0 ] && "Cancelou ou Apertou ESC." && break
@@ -53,6 +102,7 @@ while true;
                   2) TamDisco;;
                   3) PingMaquina;;
                   4) NmapScan;;
+                  5) CMDremoto;;
                   0) exit;;
           esac
 done
