cc611d25c70f48fb6fd325f7a86246701d6eb1cb
Wesllen <wesllen.maia2010@gmail.com>
2019-05-03 08:29:04 -0300

    Alterado  A forma do menu do Crud com Buttons Básicos por enquanto.

--- crud.sh
+++ crud.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 
-<<<<<<< Updated upstream
 # crud.sh - Ferramenta para controle de inventário
-=======
+#
 # crud.sh - cadastro, listagem, update e delete.
->>>>>>> Stashed changes
 #
 # Site:       https://wesllenmatias.com.br
 # Autor:      Wesllen Matias
 # Manutenção: Wesllen Matias
 #
 # ------------------------------------------------------------------------ #
-<<<<<<< Updated upstream
 # Uma ferramenta para controle de inventário.
-=======
-# Programa para cadastar e administrar inventário
->>>>>>> Stashed changes
-#
+# Programa para cadastar e administrar inventário#
 # ------------------------------------------------------------------------ #
 # Histórico:
 #
@@ -26,7 +20,6 @@
 #   bash 4.4.19
 # ------------------------------------------------------------------------ #
 
-<<<<<<< Updated upstream
 function Cad_Marca() {
   yad --title "Cadastro de Marca" \
   --width=400 \
@@ -66,24 +59,8 @@ export -f Cadastro_Modelo
 export -f Entrada
 
 yad --title "Inventário" \
---width=600 \
---height=400 \
 --center \
 --buttons-layout start \
---dialog-sep \
---form \
---field="Cadastro de Marcas":fbtn '1' \
+--button="Cadastro de Marca":"bash -c Cad_Marca" \
 --button="Cadastro de Modelo":"bash -c Cadastro_Modelo" \
---button="Cadastrar Entrada":"bash -c Entrada"
-
-case $? in
-      1) Cad_Marca;;
-esac
-=======
-while true; do
-  yad --title "Selecione uma opção:" \
-  --form \
-  --field="Opções::"CB "Cad.Modelo!Cad.Marca!Cad.tipo!Cad.Item" \
-  --rtl
-done
->>>>>>> Stashed changes
+--button="Cadastrar Entrada":"bash -c Entrada" 0 0 0
