0f5299b88df75e5c3286655f97ad890347bc13e0
WESLLEN MATIAS MAIA <46201054+WesllenMatias@users.noreply.github.com>
2019-05-05 12:18:57 -0300

    Update programas.sh

--- programas.sh
+++ programas.sh
@@ -4,15 +4,11 @@
 
 sudo apt update -y && sudo apt upgrade -y && \
 sudo apt install virtualbox -y && \
-sudo apt install remmina -y && \
-sudo apt install remmina-plugin-rdp -y && \
-sudo apt install reminna-plugin-vnc -y && \
 sudo apt install filezilla -y && \
 sudo apt install htop -y && \
 sudo apt install curl -y && \
 sudo apt install bmon -y && \
 sudo apt install telegram-desktop -y && \
-sudo apt install skypeforlinux -y && \
 sudo apt install lynx -y && \
 sudo apt install dialog -y && \
 sudo apt install jq -y && \
@@ -21,4 +17,4 @@ sudo apt install tmux -y && \
 sudo apt install screenfetch -y && \
 sudo apt install atom -y && \
 sudo apt install git -y && \
-sudo apt install gimp -y && \
+sudo apt install gimp -y
