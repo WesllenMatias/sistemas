6ab2dc3572867481292a98437401534a92487510
Wesllen <wesllen.maia2010@gmail.com>
2019-07-15 17:41:15 -0300

    Arquivo para criar ao chegar na segunda estação

---
+++ arquivo.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE diag_db;
+
+
+CREATE TABLE IF NOT EXISTS `diag_db`.`raspberrys` (
+  `idraspberrys` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `ip` VARCHAR(13) NOT NULL,
+  `usuario` VARCHAR(45) NOT NULL,
+  `senha` VARCHAR(45) NOT NULL,
+  `local` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idraspberrys`),
+  UNIQUE INDEX `idraspberrys_UNIQUE` (`idraspberrys` ASC))
+ENGINE = InnoDB
+
+
+INSERT INTO raspberrys ( nome, ip, usuario, senha, local ) VALUES ( "LJ7-VENDAS-02","192.168.17.102", "pi", "valessh", "LOJA07" );
